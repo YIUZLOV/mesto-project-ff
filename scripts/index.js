6d9ac0348aa3a61012d5c99e449c13e6a2b78ba3
Evgeniy Uzlov <yiuzlov@yandex.ru>
2024-03-11 18:40:17 +0300

    Проект выполнен

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,19 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+const placesList = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
+function deleteCard(element) {
+  element.remove();
+};
+function addCards(element) {
+  const placesItem = cardTemplate.querySelector('.places__item').cloneNode(true);
+  const deleteButton = placesItem.querySelector('.card__delete-button');
+  placesItem.querySelector('.card__image').src = element.link;
+  placesItem.querySelector('.card__title').textContent = element.name;  
+  deleteButton.addEventListener('click', function () {
+    deleteCard(placesItem);
+  });
+  return placesList.append(placesItem);
+};
+initialCards.forEach(addCards);
